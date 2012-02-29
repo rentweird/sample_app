124fc928ea1dac79aab6aa85b7ae5c8d9e7d94ec
Erik Diehl <apartmenterik@gmail.com>
2012-02-28 18:21:40 -0600

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -18,6 +19,7 @@ class UsersController < ApplicationController
     end
   end
 end
+
 
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,12 +20,12 @@ describe User do
 
   subject { @user }
 
-
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -101,6 +101,12 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
+
 
 
