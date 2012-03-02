af41208c3e59e2afa77c8c38e55dfa9e5d68f0b0
Erik Diehl <apartmenterik@gmail.com>
2012-03-01 19:50:32 -0600

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,7 +29,13 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
-
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }  
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
 
   it { should be_valid }
 
@@ -122,8 +128,30 @@ describe User do
     it { should be_admin }
   end
 
-  describe "micropost associations" do
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
 
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
+
+  describe "micropost associations" do
     before { @user.save }
     let!(:older_micropost) do 
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
@@ -153,10 +181,26 @@ describe User do
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
     end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
   end
 end
-
-
-
-
-
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,8 +13,8 @@ describe "Static pages" do
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        FactoryGirl.create(:micropost, :user => user, :content => "Lorem")
+        FactoryGirl.create(:micropost, :user => user, :content => "Ipsum")
         sign_in(user)
         visit root_path
       end
@@ -24,8 +24,19 @@ describe "Static pages" do
           page.should have_selector("tr##{item.id}", text: item.content)
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before { user.follow!(other_user) }
+
+        it { should have_selector('a', href: following_user_path(user),
+                                       content: "0 following") }
+        it { should have_selector('a', href: followers_user_path(user),
+                                       content: "1 follower") }
+      end
     end
   end
+
 
   describe "Help page" do
     before { visit help_path }
